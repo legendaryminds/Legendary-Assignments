edc21caa61736e3191e192e85d810bb5c255ffa3
LegendaryMinds <128437646+legendaryminds@users.noreply.github.com>
2023-11-20 01:54:00 -0800

    update Business-Time-Project

--- exercises/Business-Time-Project/main.js
+++ exercises/Business-Time-Project/main.js
@@ -38,13 +38,13 @@
 // </div>; */}
 
 // {/* <img src="https://d1csarkz8obe9u.cloudfront.net/embeddesigns/00ada5e159b6ad93a9e342c3592b46ce_embed.jpg" style="max-width:800px;max-height:500px; height: 100%; width: 100%;" alt="alternatetext"></img> */}
-{/* <div style="position: relative; width: 100%; height: 0; padding-top: 149.4163%;
+/* <div style="position: relative; width: 100%; height: 0; padding-top: 149.4163%;
  padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
  border-radius: 8px; will-change: transform;">
   <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
     src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFzXUgzz04&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
   </iframe>
 </div>
-<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFzXUgzz04&#x2F;view?utm_content=DAFzXUgzz04&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Design</a> by Cynthia Nicole Carter */}
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFzXUgzz04&#x2F;view?utm_content=DAFzXUgzz04&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Design</a> by Cynthia Nicole Carter */
 
-https://www.canva.com/design/DAFzXUgzz04/view
+// https://www.canva.com/design/DAFzXUgzz04/view
